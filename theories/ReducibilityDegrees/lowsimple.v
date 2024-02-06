08bde05e658902e603f4ccfdf576e4f027a95b7a
Haoyi <haze00001@stud.uni-saarland.de>
2024-02-06 01:30:52 +0100

    Add F_

--- theories/ReducibilityDegrees/lowsimple.v
+++ theories/ReducibilityDegrees/lowsimple.v
@@ -58,40 +58,172 @@ Qed.
 End LowSimplePredicate.
 
 
+Require Import List.
+Import ListNotations.
+Notation "'Σ' x .. y , p" :=
+    (sigT (fun x => .. (sigT (fun y => p)) ..))
+        (at level 200, x binder, right associativity,
+        format "'[' 'Σ'  '/  ' x  ..  y ,  '/  ' p ']'")
+    : type_scope.
+
+Require Import SyntheticComputability.Shared.FinitenessFacts.
+
 Section Construction.
 
-  Definition mu (P: nat -> Prop) n := P n /\ forall m, P m -> n <= m.
+  Definition mu n (P: nat -> Prop) := P n /\ forall m, P m -> n <= m.
 
-  Variable W: nat -> nat -> nat -> Prop.
+  Variable W_: nat -> nat -> nat -> Prop.
+  Hypothesis W_spec: forall P, semi_decidable P -> exists c, forall x, P x <-> exists n, W_ c n x.
 
-  Definition W_spec (c n x: nat) : Prop := forall P, semi_decidable P -> exists c, forall x, P x <-> exists n, W c n x.
+  Definition disj (A B: nat -> Prop) := forall x, A x -> B x -> False.
+  Definition disj__l (A: list nat) (B: nat -> Prop) := forall x, In x A -> B x -> False.
 
-  Definition disj (A B: nat -> Prop) := forall x, A x <-> ~ B x.
-  Notation "A ⊎ B" := (disj A B) (at level 30).
+  Notation "A # B" := (disj A B) (at level 30).
+  Notation "A l# B" := (disj__l A B) (at level 30).
 
-  (** ** Is this a correct definition for inifity ? **)
+  Record Extendsion :=
+  {
+      extendP: list nat -> nat -> option nat -> Prop;
+      extend_dec: forall l x, Σ y, extendP l x y;
+      extend_uni: forall l x y1 y2, extendP l x y1 -> extendP l x y2 -> y1 = y2
+  }.
 
-  Definition infity (P: nat -> Prop) := forall x, exists y, y > x -> P y.
-
-  Fixpoint P n : nat -> Prop :=
+  Fixpoint P_templete (E: (nat -> Prop) -> nat -> nat -> Prop) n x: Prop :=
     match n with
-    | O => fun _ => False
-    | S n => fun x =>
-              P n x \/ exists e, mu (fun e => e < n /\ ((W e n) ⊎ (P n)) /\ mu (fun x => W e n x /\ 2 * e < x) x) e
+    | O => False
+    | S n => P_templete E n x \/ E (P_templete E n) n x
     end.
 
-  Definition W' e x := exists n, W e n x.
-  Definition P' x := exists n, P n x.
+  Fixpoint F_ (E: Extendsion) n l: Prop :=
+    match n with
+    | O => l = []
+    | S n => exists ls a, F_ E n ls /\ (extendP E ls n a) /\ l = if a is Some a then a :: ls else ls
+    end.
 
-  Definition R P e := infity (W' e) -> exists w, (W' e w) /\ P w.
+  Definition F_with E x := exists l n, In x l /\ (F_ E n l).
 
-  Lemma P_meet_R : forall n, R P' n.
+  Lemma F_uni E : forall n l1 l2, F_ E n l1 -> F_ E n l2 -> l1 = l2 .
+  Proof.
+    induction n; simpl; [intros ?? -> ->; easy |].
+    intros l1 l2 (ls & a & H1 & H2 & ->) (ls' & a' & H1' & H2' & ->).
+    rewrite !(IHn _ _ H1 H1') in *.
+    rewrite !(extend_uni H2 H2') in *.
+    easy.
+  Qed.
+
+  Lemma F_computable E : exists f: nat -> list nat, forall n, F_ E n (f n).
+  Proof.
+    set (F := fix f x :=
+           match x with
+           | O => []
+           | S x => match projT1 ((extend_dec E) (f x) x) with
+                   | None => f x
+                   | Some a => a :: (f x)
+                   end
+           end).
+    exists F. induction n; simpl.
+    - easy.
+    - exists (F n). destruct (extend_dec E (F n) n).
+      exists x. cbn. split; [eauto|]. split; [eauto|].
+      easy.
+  Qed.
+
+  Definition decider: nat -> list nat -> bool.
+  Proof.
+    intros x l.
+    destruct (ListDec.In_dec EqDec.nat_eq_dec x l).
+    - exact true. - exact false.
+  Defined.
+
+  Fact decider_spec: forall x l, (decider x l) = true <-> In x l.
+  Proof.
+    intros x l. unfold decider.
+    destruct (ListDec.In_dec EqDec.nat_eq_dec x l); firstorder.
+  Qed.
+
+  Lemma F_with_semi_decidable E: semi_decidable (F_with E).
+  Proof.
+    unfold semi_decidable, semi_decider.
+    destruct (F_computable E) as [f Hf].
+    exists (fun x n => decider x (f n)).
+    intros x. split.
+    - intros (l & n & Hxl & Hl).
+      exists n. rewrite decider_spec.
+      now rewrite (F_uni (Hf n) Hl).
+    - intros (n & Hn%decider_spec).
+      exists (f n), n; split; eauto.
+  Qed.
+
+  Section SimpleSet.
+
+  Definition extend_simple l__n n x :=
+    exists e, mu e (fun alpha => e < n /\ (l__n l# W_ alpha n) /\ mu x (fun beta => W_ alpha n beta /\ 2 * alpha < beta)).
+
+  Definition simple_extendsion: Extendsion.
+  Proof.
+    exists (fun l n a => forall x, a = Some x <-> extend_simple l n x).
+    - intros l x. admit.
+    - intros l x y1 y2 Hy1 Hy2.
+      admit.
+  Admitted.
+
+  Definition E_simple P__n n x :=
+    exists e, mu e (fun alpha => e < n /\ (W_ alpha n # P__n) /\ mu x (fun beta => W_ alpha n beta /\ 2 * alpha < beta)).
+
+
+(*  Definition P_ := P_templete E_simple. *)
+  Definition P_ := F_ simple_extendsion.
+  Definition W e x := exists n, W_ e n x.
+  Definition P := F_with simple_extendsion.
+
+  Definition R_simple P e := (~ exhaustible (W e)) -> ~ (W e) # P.
+
+  Section StrongInduction.
+
+    Definition strong_induction (p: nat -> Type) :
+    (forall x, (forall y, y < x -> p y) -> p x) -> forall x, p x.
+    Proof.
+        intros H x; apply H.
+        induction x; [intros; lia| ].
+        intros; apply H; intros; apply IHx; lia.
+    Defined.
+
+  End StrongInduction.
+
+  Tactic Notation "strong" "induction" ident(n) := induction n using strong_induction.
+
+  Lemma P_meet_R_simple : forall e, R_simple P e.
+  Proof.
+    strong induction e.
+    intros Hex Hdisj.
+    unfold disj in Hdisj.
+  Admitted.
+
+  Lemma P_semi_decidable : semi_decidable P.
+  Proof.
+    apply F_with_semi_decidable.
+  Qed.
+
+  Lemma P_coinfinite : ~ exhaustible (compl P).
   Proof.
   Admitted.
 
-  Lemma P_semi_decidable : semi_decidable P'.
+  Lemma P_simple : simple P.
   Proof.
-  Admitted.
+    unfold simple; repeat split.
+    - rewrite EA.enum_iff. now apply P_semi_decidable.
+    - apply P_coinfinite.
+    - intros [q (Hqenum & Hqinf & Hq)].
+      rewrite EA.enum_iff in Hqenum.
+      destruct (W_spec Hqenum) as [c HWq].
+      apply (@P_meet_R_simple c).
+      intros [l Hqe]; apply Hqinf.
+      exists l. intros x Hqx. apply (Hqe x).
+      now rewrite HWq in Hqx.
+      intros x HWcx HPx. unfold W in HWcx.
+      rewrite <- HWq in HWcx. apply (Hq x HWcx HPx).
+  Qed.
 
+  End SimpleSet.
 
 End Construction.
